2eedc51dfc721e6f5d6e633b6de72a2258dd754b
Tony Mejia <bonao99@gmail.com>
2019-07-26 23:18:38 -0400

    commit from ubu desktop

--- ReadVectorPrintReversed/main.c
+++ ReadVectorPrintReversed/main.c
@@ -20,14 +20,39 @@
 int main(int argc, char** argv) 
 {
     int num[100];
-    int i,temp;
+    int i,temp, avg = 0;
+    int count = 0 ;
+ 
     
-    for(i=0; i<=99; i++)
+    for(i=1; i<=100; i++)
     {
         
-        num[i] = i +1;
-        printf("num[%d]\n", num[i]);
+        num[i-1] = i * 5; 
+       avg = avg + num[i-1];     
     }
+    printf("[");
+    
+    
+    for (int j=99; j >=0;j--)
+    {
+        count = count + 1;
+        if(j==0)
+        printf("%d",num[j]);
+        else
+        printf("%d,",num[j]);
+        if (count == 10)
+            
+        {           
+                  printf("\n"); 
+                  count = 0;
+        }
+     
+        
+    }
+    
+     printf("]\n");
+     printf("\nAVERAGE : %d",avg/100);
+          
     return (EXIT_SUCCESS);
 }
 
