ce8c3bcdad385c651190b77b7a861ff97a9e6c0d
Tony Mejia <bonao99@gmail.com>
2019-08-15 00:30:50 -0400

    commit from Desk

---
+++ CppApplication_3/main.c
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: tony
+ *
+ * Created on August 14, 2019, 10:12 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    int arr[5][5];
+    int count = 0;
+    for(int row = 0;row <= 4;row ++)
+    {
+        
+        for(int col = 0;col  <= 4;col  ++)
+        {
+            arr[row][col] = rand() % 99 +1;
+    
+        }
+    }
+    
+    for(int printinn=0;printinn <=  4; printinn++)
+    {
+        
+        for(int printout =0; printout <=  4;printout++)
+        {
+             count = count + 1;
+             
+             
+            if (printout == 5)
+            {
+                printf("\n");
+            }
+            else
+            {
+               printf("%d ",arr[printinn][printout]);
+            }
+           
+              
+        }
+        printf("\n");
+    }
+    return (EXIT_SUCCESS);
+}
+
