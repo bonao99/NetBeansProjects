f1d7c57baf0c6e4d84bcb36064e84528fa4d0717
Tony Mejia <bonao99@gmail.com>
2019-08-22 18:12:30 -0400

    rename deleteme file

---
+++ FindPositioninarray/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=deleteme
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/deleteme
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=deleteme.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/deleteme.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=deleteme
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/deleteme
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=deleteme.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/deleteme.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
