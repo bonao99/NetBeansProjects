ec2b2b46685c5d6382723b6f6c2ddd98191bc09c
Tony Mejia <bonao99@gmail.com>
2019-09-22 18:11:02 -0400

    push Humano class

---
+++ Humano/src/Estudiante.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tony
+ */
+public class Estudiante        
+        
+{
+
+ public Humano Estudiante(String elnombre,String elapellido,int laedad,String elsexo)
+        {
+            
+        }    
+}
+ 
---
+++ Humano/src/humano/Humano.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package humano;
+
+/**
+ *
+ * @author tony
+ */
+public class Humano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String nombre;
+        String apellido;
+        int edad;
+        String sexo;
+        
+  public void Humano(String elnombre,String elapellido,int laedad,String elsexo)
+  {
+      nombre = elnombre;
+      apellido = elapellido;
+      edad = laedad;
+      sexo = elsexo;
+  }
+
+  
+}
+
+}
